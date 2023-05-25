0a6cf0ccb42876fd5d80201ac095b3bfe1705d6a
abahador <93795464+abahador@users.noreply.github.com>
2023-05-25 14:29:17 -0700

    lab11debug

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,8 +3,18 @@
 // Date: 5/17/2023
 
 //$("sectionbox.append")("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
-$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>")
+$("#challenge").append("<button id='highlight-button'>Toggle Highlight</button>")
+$("#problems").append("<button id='highlight-buttons'>Toggle Highlight</button>")
+$("#results").append("<button id='highlight-buttonss'>Toggle Highlight</button>")
 
-$(".highlight-button").click(function(){
+$("#highlight-button").click(function(){
+    $(this).parent().toggleClass("special");
+})
+
+$("#highlight-buttons").click(function(){
+    $(this).parent().toggleClass("special");
+})
+
+$("#highlight-buttonss").click(function(){
     $(this).parent().toggleClass("special");
 })
